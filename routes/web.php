2b0db5ae55b26f84132f34dc84f86975734aafc9
Lab RPL <you@example.com>
2019-08-09 17:06:55 +0700

    baruu

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Auth::routes(['register' => false]);
 
 Route::get('/home','HomeController@index')->name('home');
-Route::group(['prefix' => 'admin','middleware' => ['auth','role:admin']],function(){
+Route::group(['prefix' => 'admin','middleware' => ['auth','role:superadmin']],function(){
     Route::get('/admin', function () {
         return 'hallo';
     });
